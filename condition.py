363bcbbb52ec71f41b1282bc7fdd5f12ca83499f
Riya Patel <riya.patell1204@gmail.com>
2022-07-11 02:08:01 -0700

    if else if

---
+++ condition.py
@@ -0,0 +1,8 @@
+n = int(input("number: "))  # used int to take the integer value from the user
+
+if n > 0:  # indentation to know what's inside the if statement ":"
+    print("n is positive")
+elif n < 0:  # else if
+    print("n is negative")
+else:
+    print("n is zero")
