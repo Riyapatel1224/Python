273e201ecb8a1878cf2a877341d220757229e5f9
Riya Patel <riya.patell1204@gmail.com>
2022-07-11 02:06:07 -0700

    basic

---
+++ name.py
@@ -0,0 +1,3 @@
+name = input("Name : ")          #to take inout from user
+print("hello,"+ name)            #to print hello and by adding the name inputed by the user
+print(f"hello {name}")           #formatted string to plug in values from name variable
