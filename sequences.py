e6a38ef9b06f11cca9e5fb12c0100b8ebc7fe9b2
Riya Patel <riya.patell1204@gmail.com>
2022-07-11 02:07:14 -0700

    basics

---
+++ sequences.py
@@ -0,0 +1,7 @@
+name = "Harry"
+
+print(name[0])  # print first letter of the name string
+
+
+names = ["harry", "ron", "hermione"] # print all names if we add [1] it will print the first letter of all names
+print(names)
